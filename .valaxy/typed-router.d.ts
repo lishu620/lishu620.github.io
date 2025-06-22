2d862a0cdbbafda4ebd92c2e4feaea61201a3611
m1ishu <mlishu@163.com>
2025-06-23 00:10:04 +0800

    探索博客

--- .valaxy/typed-router.d.ts
+++ .valaxy/typed-router.d.ts
@@ -30,6 +30,9 @@ declare module 'vue-router/auto-routes' {
     '/page/[page]': RouteRecordInfo<'/page/[page]', '/page/:page', { page: ParamValue<true> }, { page: ParamValue<false> }>,
     '/posts/': RouteRecordInfo<'/posts/', '/posts', Record<never, never>, Record<never, never>>,
     '/posts/hello-valaxy': RouteRecordInfo<'/posts/hello-valaxy', '/posts/hello-valaxy', Record<never, never>, Record<never, never>>,
+    '/posts/VPS/如何使用Bitwarden': RouteRecordInfo<'/posts/VPS/如何使用Bitwarden', '/posts/VPS/如何使用Bitwarden', Record<never, never>, Record<never, never>>,
+    '/posts/VPS/在1Panel上部署Bitwarden': RouteRecordInfo<'/posts/VPS/在1Panel上部署Bitwarden', '/posts/VPS/在1Panel上部署Bitwarden', Record<never, never>, Record<never, never>>,
+    '/posts/VPS/deploy_web': RouteRecordInfo<'/posts/VPS/deploy_web', '/posts/VPS/deploy_web', Record<never, never>, Record<never, never>>,
     '/tags/': RouteRecordInfo<'/tags/', '/tags', Record<never, never>, Record<never, never>>,
   }
 }
